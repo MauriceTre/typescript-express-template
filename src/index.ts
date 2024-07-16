5600235c77f051622861468c4c5e84f49b3e0ac5
MauriceTre <140593975+MauriceTre@users.noreply.github.com>
2024-07-16 13:16:52 +0200

    add swagger

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,38 @@
-import todoSequelize from "./database/setup/database";
-import app from "./server";
-
-// Zugriff auf Umgebungsvariablen
+import todoSequelize from './database/setup/database';
+import server from './server';
+import swaggerUi from 'swagger-ui-express';
+import express from 'express';
 // const { PORT } = process.env;
 const PORT = process.env.PORT;
 
 todoSequelize
   .sync()
   .then(() => {
-    console.log("DB has been successfully initialized");
+    console.log('DB has been successfully initialized');
   })
-  .catch((e) => {
+  .catch(e => {
     console.log(e);
   });
 
+if (process.env.NODE_ENV === 'dev') {
+  server.use(express.static('docs'));
+  server.use(
+    '/swagger',
+    swaggerUi.serve,
+    swaggerUi.setup(undefined, {
+      swaggerOptions: {
+        url: '/swagger.json',
+      },
+    }),
+  );
+  console.log(
+    `Swagger launched on at https://localhost:${
+      process.env.PORT ?? ''
+    }/swagger`,
+  );
+}
+
 // App hört im folgenden auf den Port, welcher über die Umgebungsvariable definiert ist
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
